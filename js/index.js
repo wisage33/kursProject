2d039e02c897306c03d8118df4ff0c2753a6f72d
wisage33 <81797166+wisage33@users.noreply.github.com>
2024-03-12 09:58:20 +0000

    гейство

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// let sum = 0;
+
+// while(true) {
+
+//     let value = +prompt('Введите число:')
+
+//     if (!value) break;
+
+//     sum += value
+// }
+// alert("Сумма: " + sum)
+
+// for (let i = 0; i < 10; i++) {
+//     if (i / 5 == 1) continue;
+
+//     console.log(i)
+// }
+
+for (let i = 0; i < 10; i++) {
+
+    if(i % 2){
+        console.log(i)
+    }
+}
